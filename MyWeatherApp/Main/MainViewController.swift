738f3972441674abd67bf589133602c594971e53
dolcecyat <k14881488kk@mail.ru>
2024-11-28 21:06:49 +0300

    navigation bar done

--- MyWeatherApp/Main/MainViewController.swift
+++ MyWeatherApp/Main/MainViewController.swift
@@ -6,14 +6,19 @@
 //
 
 import UIKit
-
+enum Constants {
+    static let imageNameForLeftBarButtonItem = "gearshape.fill"
+}
 protocol MainViewProtocol: AnyObject{
     var presenter: MainPresenterProtocol? { get set }
 }
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, MainViewProtocol {
     var presenter: MainPresenterProtocol?
     
+    //MARK: - UI properties
+    
+    var searchBar = UISearchBar()
     
     // MARK: - Init
     
@@ -24,18 +29,44 @@ class MainViewController: UIViewController {
     required init?(coder aDecoder: NSCoder){
         super.init(coder: aDecoder)
     }
+    //MARK: - View lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
     }
 
+    // MARK: - Setup
+    
     private func setup() {
         MainModuleBuilder.build(self)
+        setupNavBar()
+        setDelegates()
     }
-
+    
+    private func setupNavBar() {
+        self.navigationController?.navigationBar.barTintColor = .clear
+        self.navigationController?.navigationBar.tintColor = .white
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: Constants.imageNameForLeftBarButtonItem), style: .plain, target: self, action: #selector(openSettings))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "location.fill"), style: .plain, target: self, action: #selector(getLocation))
+        navigationItem.titleView = searchBar
+    }
+    
+    @objc private func openSettings() {
+        
+    }
+    
+    @objc private func getLocation() {
+        
+    }
+    
+    private func setDelegates() {
+        searchBar.delegate = self
+    }
 }
 
-extension MainViewController: MainViewProtocol {
+private extension MainViewController {
     
+}
+extension MainViewController: UISearchBarDelegate {
 }
